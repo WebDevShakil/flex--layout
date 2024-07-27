1d5bc6d988f9822af5814967ef59895d01771be8
WebDevShakil <skshakilppki123@gmail.com>
2024-07-27 22:08:21 +0600

    grid done

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-onclick = document.getElementById("search").document.write("hello")
+let arry ={ 
+    names:"shakil",
+    name:"Ahmed",
+    age:14,
+    work:{
+        oneWork:"Free-lancer",
+        twoWork:"Banker"
+    }
+}
+arry.uname="khalid"+ " "+ "hacker"
+Object.seal(arry)
+arry = {}
+arry.addd =435346
+console.log(arry)
